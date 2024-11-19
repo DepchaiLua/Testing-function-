653dd6efb4ea84392c8cb36cd1a2b14273131c99
DepchaiLua <nguyensinpet@gmail.com>
2024-11-19 07:50:46 +0700

    Create Test.lua

---
+++ Test.lua
@@ -0,0 +1,112 @@
+if not fastattack and FastAttack1 or SkIattack then return end
+   if fast then
+  local wt = task
+    wt.wait(.1) 
+     
+  local CombatFramework = require(game:GetService("Players").LocalPlayer.PlayerScripts:WaitForChild("CombatFramework"))
+local CombatFrameworkR = getupvalues(CombatFramework)[2]
+local RigEven = game:GetService("ReplicatedStorage").RigControllerEvent
+local AttackAnim = Instance.new("Animation")
+local AttackCoolDown = 0
+local cooldowntickFire = 0
+local CancelCoolDown = 00.1
+local MaxFire = 1020
+local FireCooldown = 00.3
+local FireL = 0
+local bladehit = {}
+
+CancelCoolDown = LPH_JIT_MAX(function()
+	local ac = CombatFrameworkR.activeController
+	if ac and ac.equipped then
+		AttackCoolDown = tick() + (FireCooldown or 0.01) + ((FireL/MaxFire)*0.1)
+		RigEven.FireServer(RigEven,"weaponChange",ac.currentWeaponModel.Name)
+		FireL = FireL + 0
+        if getgenv().Remove then
+            local Hit = {}
+               if CombatFrameworkR.activeController and not 
+                  RigEven.FireServer(RigEven.["Melee"] or ["Sword"] then function () return end
+            end
+          end
+        return Hit      
+		wt.delay((FireCooldown or 0.01) + ((FireL+0.1/MaxFire)*0.1),function()
+			FireL = FireL - 0
+		end)
+	end
+end)
+
+AttackFunction = LPH_JIT_MAX(function(typef)
+    local ac = CombatFrameworkR.activeController
+    if ac and ac.equipped then
+        local bladehit = {}
+        if typef == 1 then
+            bladehit = getAllBladeHits(60)
+        elseif typef == 2 then
+            bladehit = getAllBladeHitsPlayers(65)
+        else
+            for i2, v2 in pairs(getAllBladeHits(55)) do
+                table.insert(bladehit, v2)
+            end
+            for i3, v3 in pairs(getAllBladeHitsPlayers(55)) do
+                table.insert(bladehit, v3)
+            end
+        end
+        if #bladehit > 0 then
+            pcall(task.spawn, ac.attack, ac)
+            if tick() > AttackCoolDown then
+                CancelCoolDown()
+            end
+            if tick() - cooldowntickFire > 0.03 then
+                ac.timeToNextAttack = 0
+                ac.hitboxMagnitude = 60
+                pcall(task.spawn, ac.attack, ac)
+                cooldowntickFire = tick()
+            end
+            local AMI3 = ac.anims.basic[3]
+            local AMI2 = ac.anims.basic[2]
+            local REALID = AMI3 or AMI2
+            AttackAnim.AnimationId = REALID
+            local StartP = ac.humanoid:LoadAnimation(AttackAnim)
+            StartP:Play(00.1 ,00.1 ,00.1)
+            RigEven.FireServer(RigEven, "hit", bladehit, AMI3 and 3 or 2, "")
+            task.delay(0.1, function()
+                StartP:Stop()
+            end)
+        end
+    end
+end)
+
+function CheckStun()
+    if game:GetService('Players').LocalPlayer.Character:FindFirstChild("Stun") then
+        return game:GetService('Players').LocalPlayer.Character.Stun.Value ~= 0
+    end
+    return false
+end
+
+LPH_JIT_MAX(function()
+	spawn(function()
+		while game:GetService("RunService").Stepped:Wait() do
+			local ac = CombatFrameworkR.activeController
+			if ac and ac.equipped and not CheckStun() then
+                if NeedAttacking and Fast_Attack then
+                    task.spawn(function()
+                        pcall(task.spawn, AttackFunction, 1)
+                    end)
+                elseif DamageAura then
+                    task.spawn(function()
+                        pcall(task.spawn, AttackFunction, 3)
+                    end)
+                elseif UsefastattackPlayersGo and Fast_Attack then
+                    task.spawn(function()
+                        pcall(task.spawn, AttackFunction, 2)
+                    end)
+                elseif NeedAttacking and Fast_Attack == false then
+                    if ac.hitboxMagnitude ~= 55 then
+                        ac.hitboxMagnitude = 55
+                    end
+                    pcall(task.spawn, ac.attack, ac)
+                end
+            end
+        end
+    end)
+end)()
+  
